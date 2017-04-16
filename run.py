15e1e9202f04de1e0b90d6b9c50ee8bc191f1326
cgianmarco <c.giammy95@gmail.com>
2017-04-16 20:26:28 +0200

    more tags

--- run.py
+++ run.py
@@ -1,13 +1,13 @@
 from core.dataset import *
 from core.taggers import SingleWordTagger, DoubleWordTagger
 
-n_single_tags = 20
-n_double_tags = 100
+n_single_tags = 300
+n_double_tags = 300
 
 dataset = Dataset()
 
 swt = SingleWordTagger(dataset)
-swt.run(n_single_tags)
+swt.run(n_single_tags, mf=False)
 
 dwt = DoubleWordTagger(dataset)
 dwt.run(n_double_tags)
